f4f93644ffd37fb96832151d80fe819fc32efe43
subramanyam499 <subramanyam499@gmail.com>
2023-08-24 19:45:12 -0700

    weblogic-install-domain

--- domain.py
+++ domain.py
@@ -1,6 +1,6 @@
-#!/u01/app/oracle/may23/new_middlware/oracle_common/common/bin/wlst.sh
+#!/u01/app/oracle/may23/new_middlware/wls/oracle_common/common/bin/wlst.sh
 import os, sys
-readTemplate('/u01/app/oracle/may23/new_middlware/wlserver/common/templates/wls/wls.jar')
+readTemplate('/u01/app/oracle/may23/new_middlware/wls/wlserver/common/templates/wls/wls.jar')
 cd('/Security/base_domain/User/weblogic')
 cmo.setPassword('weblogic1')
 cd('/Server/AdminServer')
@@ -8,7 +8,7 @@ setOption('ServerStartMode', 'prod')
 cmo.setName('admin_test')
 cmo.setListenPort(7001)
 cmo.setListenAddress('192.168.43.44')
-writeDomain('/u01/app/oracle/test/middleware/user_projects/domains/jenkins_test')
+writeDomain('/u01/app/oracle/may23/new_middlware/wls/user_projects/domains/jenkins_billing')
 closeTemplate()
 print '>>>Domain created successfully>>>'
 exit()
